6cb69365a6b85c9df9a1e213bf2301b754918482
koryunavdalyan <koryunavdalyan@gmail.com>
2022-12-24 19:38:22 +0400

    homework2

---
+++ src/homework2/ArrayUtil.java
@@ -0,0 +1,42 @@
+package homework2;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class ArrayUtil {
+    public static void main(String[] args) {
+        int[] array = {77, 86, 100, 405, 12, 78, 7, 23, 4, 10};
+        for (int i = 0; i < 10; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+        int min = array[0];
+            for (int i = 0; i < array.length; i++);
+        {
+            if (min > array['i']) {
+                min = array['i'];
+
+            }
+            System.out.println();
+            System.out.println("min is:" +min);
+            int max = array[0];
+            for (int i = 0; i < array.length; i++){
+                if(max < array [i]); {
+                    max= array[i];
+
+
+                }
+                System.out.println();
+                System.out.println("max is; ' + max);");
+
+            }
+
+
+            }
+
+            }
+
+
+        }
+
+
